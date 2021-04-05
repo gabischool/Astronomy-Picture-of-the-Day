4da2272ab76dee038aefea16136601dbf05588e3
kingjama <fadumamukhtar95@gmail.com>
2021-04-05 04:10:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios"
+
+import NASA from "./NASA"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row, Container, Col } from "react-bootstrap";
 
 function App() {
+  const [nasa, setNasa]= useState([])
+  useEffect(()=> {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=77k19H4LulMCZJOvU6MFEDNEd74j706mwB9mmoBh")
+    .then(res =>
+        {
+        
+
+         setNasa(res.data)}).catch(err => console.log(err))})
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+     <NASA nasaProp ={nasa}/>
+     
     </div>
+
+    
   );
 }
 
